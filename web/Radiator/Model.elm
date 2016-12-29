13b6043d75342cb6dbc0041015063bdef0d5ddf6
egkoppel <egluschovekoppel1@gmail.com>
2016-12-29 09:55:16 +0000

    Move to web folder
    This is currently testing

---
+++ web/Radiator/Model.elm
@@ -0,0 +1,49 @@
+module Radiator.Model exposing (..)
+
+import Travis
+
+
+type Msg = RefreshBuilds 
+            | NewBuildStatus (Maybe (String, Travis.BranchStatus))
+            | FlipConfigMode 
+            | UpdateRepositoryField String
+            | AddRepository
+            | RemoveRepository String
+            | UpdateApiKeyField String 
+            | TogglePrivateTravis Bool
+            | SaveApiKey
+
+
+type alias Model = {
+  mode: AppMode,
+  configuration: Configuration,
+  configPanel: ConfigPanel,
+  buildStatus : List RadiatorStatus
+}
+
+type alias RadiatorStatus = {
+  repository: String,
+  branch: Maybe String,
+  state: String
+}
+
+
+type alias Configuration = {
+  apiKey: Maybe String,
+  repositories: List String
+}
+
+
+type alias ConfigPanel = {
+  repositorySlug: String,
+  apiKeyValue: String
+}
+
+
+type alias BuildStatus = {
+  branch: String,
+  state: String
+}
+
+
+type AppMode = Monitoring | Config
---
+++ web/Radiator/Update.elm
@@ -0,0 +1,105 @@
+module Radiator.Update exposing (update, refreshBuilds)
+
+import String
+import Task
+import Platform.Cmd exposing (Cmd)
+import Radiator.Ports as Ports
+
+import Travis
+import Radiator.Model as Model exposing (..)
+import Util
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update action model =
+  case action of
+     RefreshBuilds ->
+       (model, refreshBuilds model.configuration)
+
+     NewBuildStatus (Just build) ->
+       (refreshModelBuildState build model, Cmd.none)
+
+     NewBuildStatus Nothing -> (model, Cmd.none)
+
+     FlipConfigMode ->
+       ({ model | mode = (flipAppMode model.mode) }, Cmd.none)
+
+     UpdateRepositoryField repository ->
+       let cfg = model.configPanel
+           configView = { cfg | repositorySlug = repository }
+       in ({ model | configPanel = configView }, Cmd.none)
+
+     AddRepository ->
+       let currentConfig = model.configuration
+           cfgPanel = model.configPanel
+           updatedRepositories = List.append model.configuration.repositories [model.configPanel.repositorySlug]
+           updatedModel = ({ model | configuration = { currentConfig | repositories = updatedRepositories }, configPanel = { cfgPanel | repositorySlug = "" } })
+       in (updatedModel, updateConfigurationCmd updatedModel.configuration)
+
+     RemoveRepository repository ->
+       let newRepositories = List.filter (\r -> r /= repository) model.configuration.repositories
+       in updateConfig (\cfg -> { cfg | repositories = newRepositories }) model
+
+     TogglePrivateTravis usePrivateTravis ->
+       let newApiKey = if usePrivateTravis 
+                         then Just model.configPanel.apiKeyValue
+                         else Nothing
+       in updateConfig (\cfg -> { cfg | apiKey = newApiKey }) model
+
+     UpdateApiKeyField key ->
+       let cfg = model.configPanel
+           configView = { cfg | apiKeyValue = key }
+       in ({ model | configPanel = configView }, Cmd.none)
+
+     SaveApiKey -> 
+       updateConfig (\cfg -> {cfg | apiKey = (Just model.configPanel.apiKeyValue)}) model
+
+
+updateConfig: (Configuration -> Configuration) -> Model -> (Model, Cmd Msg)
+updateConfig f model =
+  let cfg = model.configuration
+      updatedModel = { model | configuration = f cfg }
+  in (updatedModel, updateConfigurationCmd updatedModel.configuration)
+
+updateConfigurationCmd: Model.Configuration -> Cmd Msg
+updateConfigurationCmd config =
+  Cmd.batch [Ports.saveConfiguration config, refreshBuilds config]
+
+refreshModelBuildState: (String, Travis.BranchStatus) -> Model -> Model 
+refreshModelBuildState newStatus model =
+  let newBuildStatuses = toRadiatorStatusList <| toBuildStatusList newStatus
+      radiatorStatuses = 
+        List.filter (\x -> x.repository /= fst newStatus) model.buildStatus
+         |> List.append newBuildStatuses
+      updatedBuildStatus = List.concatMap (\r -> List.filter (\s -> s.repository == r) radiatorStatuses) model.configuration.repositories
+  in { model | buildStatus = updatedBuildStatus }
+
+
+toRadiatorStatusList: (String, List BuildStatus) -> List RadiatorStatus
+toRadiatorStatusList (repository, branchBuildStatuses) =
+  let nonPassed = List.filter (\build -> build.state /= "passed") (List.take 5 branchBuildStatuses)
+  in case nonPassed of
+    [] -> [RadiatorStatus repository Nothing "passed"]
+    xs -> List.map (\build -> RadiatorStatus repository (Just build.branch) build.state) nonPassed
+
+
+toBuildStatusList: (String, Travis.BranchStatus) -> (String, List BuildStatus)
+toBuildStatusList (repositoryName, {branches, commits}) = 
+  (repositoryName, List.map2 combineAsBuildStatus branches commits)
+
+
+combineAsBuildStatus: Travis.BranchBuild -> Travis.Commit -> BuildStatus
+combineAsBuildStatus { state } { branch } = { state = state, branch = branch }
+
+
+refreshBuilds : Configuration -> Cmd Msg 
+refreshBuilds { apiKey, repositories } =
+  let repositoryTasks repository = Travis.getBranchBuildStatus apiKey repository
+  in List.map repositoryTasks repositories
+        |> List.map (Task.perform NewBuildStatus NewBuildStatus)
+        |> Cmd.batch
+
+
+flipAppMode: AppMode -> AppMode
+flipAppMode mode = case mode of 
+  Monitoring -> Config
+  Config -> Monitoring
